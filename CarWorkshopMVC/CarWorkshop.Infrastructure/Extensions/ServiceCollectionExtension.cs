5dcb4b513b9530c76c805f851276897aecf8fafe
MikolajKusinski <mikolajk558@gmail.com>
2024-09-17 16:44:27 +0200

    Configured Db connection and added seeder

---
+++ CarWorkshopMVC/CarWorkshop.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+using CarWorkshop.Infrastructure.Persistence;
+using CarWorkshop.Infrastructure.Seeders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarWorkshop.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.AddDbContext<CarWorkshopDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("CarWorkshopConnectionString"))
+        );
+
+        services.AddScoped<CarWorkshopSeeder>();
+    }
+}
